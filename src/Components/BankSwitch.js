702d3234ff8f3823a66a23739a7efa7158073740
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-10 12:04:26 +0100

    button components for changing sound kits added

---
+++ src/Components/BankSwitch.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const BankSwitch = ({ changeKit }) => {
+    return (
+        <div id="bank-switch-panel">
+
+            <button
+                 id='bankOne' 
+                 className='bank-btn'
+                 onClick={ changeKit }
+                >
+                Bank One
+            </button>
+
+            <button
+                id='bankTwo' 
+                className='bank-btn'
+                onClick={ changeKit }
+                >
+                Bank Two
+            </button>
+            
+        </div>
+    )
+}
+
+export default BankSwitch;
