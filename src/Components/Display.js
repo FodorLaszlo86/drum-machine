7fffd4420ab5d26ec5fb9bcc427ba71c45d8945b
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-02-10 20:59:12 +0100

    jsx markup restructured for better styling

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Display = ({ soundName, activeKit }) => {
     return (
-        <div id='display'>
-            <div id='activekit-display'>{ activeKit }</div>
-            <div id="display-sound-name">{ soundName }</div>
+        <div id='display' className='display'>
+            <div class='display__secondary'>
+                <div id='activekit-display' className='display__activekit'>{ activeKit }</div>
+                <div className='display__volume'>Vol: 32</div>
+            </div>
+            <div id="display-sound-name" className='display__primary'>{ soundName }</div>
         </div>
     )
 }
